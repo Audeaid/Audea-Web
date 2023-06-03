740d09202bdc9d8a9d8c68beee490d7a5f0b9b58
Furqon Wilogo <fdwilogo@gmail.com>
2023-06-03 21:05:20 +0700

    make items centered

--- app/app/accounts/page.tsx
+++ app/app/accounts/page.tsx
@@ -1,5 +1,9 @@
 import { UserProfile } from '@clerk/nextjs';
 
 export default function Page() {
-  return <UserProfile />;
+  return (
+    <main className="min-w-screen min-h-screen flex items-center justify-center pb-20">
+      <UserProfile />
+    </main>
+  );
 }
