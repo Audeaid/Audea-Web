5a8826687c102d28b00dde067ba58f75fdd6abbd
Furqon Wilogo <fdwilogo@gmail.com>
2023-06-13 11:41:36 +0300

    fix edit account

--- app/app/accounts/[[...accounts]]/page.tsx
+++ app/app/accounts/[[...accounts]]/page.tsx
@@ -1,62 +1,16 @@
-'use client';
-
-import { UserProfile, auth, useClerk } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
 import './styles.css';
-import { useEffect } from 'react';
 import { redirect } from 'next/navigation';
+import Client from './client';
 
 export default function Page() {
-  const { loaded } = useClerk();
-
   const { userId: clerkUserId } = auth();
 
   if (!clerkUserId) redirect('/login');
 
-  useEffect(() => {
-    if (loaded) {
-      setTimeout(() => {
-        const paragraphs = document.getElementsByTagName('p');
-        for (let i = 0; i < paragraphs.length; i++) {
-          paragraphs[i].id = 'clerkKontolGuaGabisaStylingNgentot';
-        }
-      }, 2000);
-    }
-  }, [loaded]);
-
   return (
     <section className="w-fit h-fit flex items-center justify-start mx-auto text-foreground">
-      <UserProfile
-        path="/app/accounts"
-        routing="path"
-        appearance={{
-          elements: {
-            card: 'border-0 shadow-none rounded-none',
-            navbar: 'hidden',
-            scrollBox: 'text-foreground bg-background rounded-none',
-            pageScrollBox:
-              'text-foreground bg-background rounded-none pt-0 px-0 pb-4',
-            page: 'text-foreground bg-background',
-            profilePage: 'text-foreground',
-            header: 'text-foreground',
-            headerTitle: 'text-foreground',
-            headerSubtitle: 'text-foreground',
-            profileSectionTitleText: 'text-foreground',
-            profileSectionTitle: 'border-border',
-            userPreviewMainIdentifier: 'text-foreground',
-            accordionTriggerButton: 'text-foreground',
-            accordionContent: 'text-foreground',
-            breadcrumbs: 'hidden',
-            formField: 'text-foreground',
-            formFieldLabel: 'text-foreground',
-            form: 'text-foreground',
-            formFieldSuccessText: 'text-foreground',
-            formHeader: 'text-foreground',
-            formHeaderSubtitle: 'text-foreground',
-            otpCodeFieldInput: 'border-border',
-            navbarMobileMenuButton: 'hidden',
-          },
-        }}
-      />
+      <Client />
     </section>
   );
 }
