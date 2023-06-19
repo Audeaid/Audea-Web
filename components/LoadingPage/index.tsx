35b60b825fdeb3118f818ec11c030267fd4fc21e
Furqon Wilogo <fdwilogo@gmail.com>
2023-06-19 11:33:36 +0700

    fix loading suspense

--- components/LoadingPage/index.tsx
+++ components/LoadingPage/index.tsx
@@ -1,6 +1,6 @@
 export default function LoadingPage() {
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
+    <div className="min-h-screen min-w-screen flex items-center justify-center fixed inset-0 z-0">
       <p className="text-4xl loading-animation">Audea</p>
     </div>
   );
