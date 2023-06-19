8d7b26dc5913ea71aa302e5d40bc775d15625c5c
Furqon Wilogo <fdwilogo@gmail.com>
2023-06-19 11:22:26 +0700

    fix bug

--- app/notallowed/page.tsx
+++ app/notallowed/page.tsx
@@ -3,27 +3,38 @@ import { redirect } from 'next/navigation';
 import Client from './lib';
 import { auth } from '@clerk/nextjs';
 import { signJwt } from '@/utils/jwt';
+import { generateUrl } from '@/utils/url';
 
 export default async function Page() {
-  const { userId: clerkUserId } = auth();
+  try {
+    const { userId: clerkUserId } = auth();
 
-  if (!clerkUserId) throw new Error('clerkUserId is null');
+    if (!clerkUserId) throw new Error('clerkUserId is null');
 
-  const token = signJwt(clerkUserId);
+    const token = signJwt(clerkUserId);
 
-  const { endDate, type, extended } = await checkSubscription(token);
+    const { endDate, type, extended } = await checkSubscription(token);
 
-  const subscriptionEnd = new Date() >= new Date(endDate);
+    const subscriptionEnd = new Date() >= new Date(endDate);
 
-  if (subscriptionEnd) {
-    return (
-      <Client
-        subscriptionType={type}
-        hasExtendedTrial={extended}
-        token={token}
-      />
+    if (subscriptionEnd) {
+      return (
+        <Client
+          subscriptionType={type}
+          hasExtendedTrial={extended}
+          token={token}
+        />
+      );
+    } else {
+      return redirect('/app');
+    }
+  } catch (error) {
+    const e = JSON.stringify(error);
+    const url = generateUrl(
+      `/error?message=${encodeURIComponent(e)}&from=${encodeURIComponent(
+        `/notallowed`
+      )}`
     );
-  } else {
-    redirect('/app');
+    return redirect(url.href);
   }
 }
