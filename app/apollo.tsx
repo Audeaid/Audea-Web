a1ec7fdecb50432b27fc447eee16babd91a9a619
Furqon Wilogo <fdwilogo@gmail.com>
2023-07-29 19:44:26 +0700

    add different manifest

--- app/apollo.tsx
+++ app/apollo.tsx
@@ -2,7 +2,20 @@
 
 import client from '@/utils/graphql'
 import { ApolloProvider } from '@apollo/client'
+import { useLayoutEffect } from 'react'
 
 export function ApolloNextClient({ children }: { children: React.ReactNode }) {
+  useLayoutEffect(() => {
+    const isAppleDevice = /Mac|iPhone|iPad|iPod/i.test(navigator.userAgent)
+
+    const manifestTag = document.querySelector('link[rel="manifest"]') as HTMLLinkElement
+
+    if (!manifestTag) return
+
+    if (isAppleDevice) {
+      manifestTag.href = '/manifest-apple.json'
+    }
+  }, [])
+
   return <ApolloProvider client={client}>{children}</ApolloProvider>
 }
