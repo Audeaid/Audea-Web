26b067621759549a53906e34bdff6d3c899db6cf
Furqon Wilogo <fdwilogo@gmail.com>
2023-07-29 21:01:46 +0700

    fix if manifest

--- app/apollo.tsx
+++ app/apollo.tsx
@@ -7,15 +7,17 @@ import { useEffect } from 'react'
 export function ApolloNextClient({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const isAppleDevice = /Mac|iPhone|iPad|iPod|Macintosh|Apple/i.test(navigator.userAgent)
-    const isIPhoneOrIPad = isAppleDevice && navigator.maxTouchPoints > 0
+    const hasTouchPoint = navigator.maxTouchPoints > 0
 
     const manifestLink = document.createElement('link')
     manifestLink.rel = 'manifest'
 
     if (isAppleDevice) {
-      manifestLink.href = '/manifest-apple.json'
-    } else if (isIPhoneOrIPad) {
-      manifestLink.href = '/manifest-ios.json'
+      if (hasTouchPoint) {
+        manifestLink.href = '/manifest-ios.json'
+      } else {
+        manifestLink.href = '/manifest-apple.json'
+      }
     } else {
       manifestLink.href = '/manifest.json'
     }
